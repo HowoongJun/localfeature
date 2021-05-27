50d329f43837c918b2472f917452ad50a5721be7
HowoongJun <prestoxic@gmail.com>
2021-05-27 19:43:04 +0900

    [MOD] train code edited - rotation (data augmentation)

--- EventPointNet/train.py
+++ EventPointNet/train.py
@@ -5,6 +5,7 @@ from torch.utils.data import DataLoader
 from common.Log import DebugPrint
 import DBhandler.DBhandler as DBhandler
 import numpy as np
+import cv2
 
 class CTrain():
     def __init__(self, learningRate = 0.001):
@@ -12,10 +13,11 @@ class CTrain():
         self.__learningRate = learningRate
         self.__sPrintStep = 100
         self.__batch_size = 4
-        self.__strCkptPath = "./EventPointNet/checkpoints"
-        if(not os.path.exists(self.__strCkptPath)):
+        self.__strCkptPath = "./EventPointNet/checkpoints/checkpoint.pth"
+        if(not os.path.exists(os.path.dirname(self.__strCkptPath))):
             os.mkdir(self.__strCkptPath)
-    
+        
+        
     def Open(self, db, dbPath):
         self.__dbHandler = DBhandler.CDbHandler(db)
         self.__dbHandler.Open(dbPath)
@@ -29,6 +31,8 @@ class CTrain():
         # self.__loss = torch.nn.CrossEntropyLoss()
         self.__loss = torch.nn.MSELoss()
         self.__optimizer = torch.optim.Adam(self.__model.parameters(), lr = self.__learningRate)
+        if(os.path.isfile(self.__strCkptPath)):
+            self.__model.load_state_dict(torch.load(self.__strCkptPath))
 
     def __train(self):
         self.__model.train(True)
@@ -37,16 +41,18 @@ class CTrain():
         for sBatch, data in enumerate(self.__train_loader):
             image, target = data['image'].to(self.__device, dtype=torch.float32), data['target'].to(self.__device, dtype=torch.float32)
 
-            self.__model.zero_grad()
-            output, _ = self.__model.forward(image)
-            output = output[:, :-1, :]
-            output = torch.nn.functional.pixel_shuffle(output, 8)
+            for i in range(0, 2):
+                self.__model.zero_grad()
+                output, _ = self.__model.forward(image)
+                output = output[:, :-1, :]
+                output = torch.nn.functional.pixel_shuffle(output, 8)
 
-            loss = self.__loss(output, target)
-            loss.backward()
-            self.__optimizer.step()
-            if sTrainIdx % self.__sPrintStep == 0:
-                DebugPrint().info("Step: " + str(sTrainIdx) + ", Loss: " + str(loss.item()))
-            sTrainIdx += 1
-        sTrainIdx = 0
-        torch.save(self.__model.state_dict(), self.__strCkptPath + "/checkpoint.pth")
+                loss = self.__loss(output, target)
+                loss.backward()
+                self.__optimizer.step()
+                if sTrainIdx % self.__sPrintStep == 0:
+                    DebugPrint().info("Step: " + str(sTrainIdx) + ", Loss: " + str(loss.item()))
+                sTrainIdx += 1
+
+                image, target = data['rotimage'].to(self.__device, dtype=torch.float32), data['rottarget'].to(self.__device, dtype=torch.float32)
+        torch.save(self.__model.state_dict(), self.__strCkptPath)
