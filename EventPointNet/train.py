c90db86a1e2be22bda8d0046756b8a1b24d87185
HowoongJun <prestoxic@gmail.com>
2021-05-31 11:11:46 +0900

    [MOD] cuda status check

--- EventPointNet/train.py
+++ EventPointNet/train.py
@@ -6,6 +6,7 @@ from common.Log import DebugPrint
 import DBhandler.DBhandler as DBhandler
 import numpy as np
 import cv2
+from common.utils import CudaStatus
 
 class CTrain():
     def __init__(self, learningRate = 0.001):
@@ -28,11 +29,14 @@ class CTrain():
     
     def Setting(self):
         self.__model = nets.CEventPointNet().to(self.__device)
-        # self.__loss = torch.nn.CrossEntropyLoss()
         self.__loss = torch.nn.MSELoss()
         self.__optimizer = torch.optim.Adam(self.__model.parameters(), lr = self.__learningRate)
         if(os.path.isfile(self.__strCkptPath)):
-            self.__model.load_state_dict(torch.load(self.__strCkptPath))
+            if(self.__device == "cuda"):
+                self.__model.load_state_dict(torch.load(self.__strCkptPath))
+                self.__cudaStatus = CudaStatus()
+            else:
+                self.__model.load_state_dict(torch.load(self.__strCkptPath, map_location=torch.device("cpu")))
 
     def __train(self):
         self.__model.train(True)
@@ -52,6 +56,8 @@ class CTrain():
                 self.__optimizer.step()
                 if sTrainIdx % self.__sPrintStep == 0:
                     DebugPrint().info("Step: " + str(sTrainIdx) + ", Loss: " + str(loss.item()))
+                    if(self.__device == "cuda"):
+                        DebugPrint().info("Cuda Status: " + str(self.__cudaStatus["allocated"]) + "/" + str(self.__cudaStatus["total"]))
                 sTrainIdx += 1
 
                 image, target = data['rotimage'].to(self.__device, dtype=torch.float32), data['rottarget'].to(self.__device, dtype=torch.float32)
