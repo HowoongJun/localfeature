2e386c7ce1e1fd59c9dc2b26e20d422af7349d7e
HowoongJun <prestoxic@gmail.com>
2021-06-08 19:59:51 +0900

    Revert "[ADD] docker run bash"
    This reverts commit c54b899db39a52094df3a94add7328398e109619.

--- docker.bash
+++
@@ -1,1 +0,0 @@
-docker run --gpus all -it --name eventpointnet -v /home/howoongjun/Workspace:/root/Workspace/ -e DISPLAY=docker.for.mac.host.internal:0 -p 7777:7777 howoongjun/eventpointnet:base /bin/bash 
